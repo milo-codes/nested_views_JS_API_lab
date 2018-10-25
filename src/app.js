4d2f44fc70f54ee6d82a7e88af06cbf29652f97a
Genna Walsh <gennawalsh@gmail.com>
2018-10-25 13:24:35 +0100

    app.js new Munros object and new MunrosListView set up

--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+const MunroListView = require("./views/munros_list_view.js");
+const Munros = require("./models/munros.js");
+
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript Loaded');
-})
+
+  // create and assign list view
+  const munroListContainer = document.querySelector("#munro-list");
+  const munroListView = new MunroListView(munroListContainer);
+  // get container to listen for API data
+  munroListView.bindEvents();
+
+  // create munros object and get data from API
+  const munros = new Munros();
+  munros.getData();
+});
